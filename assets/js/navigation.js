99c52476db8627f3ebcbce04f2937dd51599f1bb
AngusTse <7519212+AngusTse@users.noreply.github.com>
2025-04-28 15:06:27 +1000

    add chinse version for grandmaster

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('includes/navigation.html')
+    fetch('/includes/navigation.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('navigation-placeholder').innerHTML = data;
