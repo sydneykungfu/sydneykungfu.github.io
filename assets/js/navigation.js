b416c714cba11b8d7942de7d4e97112262f6b309
AngusTse <7519212+AngusTse@users.noreply.github.com>
2025-10-18 20:46:10 +1100

    update navigation

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -21,5 +21,37 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
             });
         })
-        .catch(error => console.error('Error loading navigation:', error));
+        .catch(error => console.error('Error loading navigation:', error))
+        .then(() => {
+            // Dynamically set Home link based on path
+            const homeLink = document.getElementById('home-link');
+            const enrollLink = document.getElementById('enroll-link');
+            const localClassLink = document.getElementById('local-class-link');
+            const aboutSifuAngusLink = document.getElementById('about-sifu-angus-link');
+            const classContentLink = document.getElementById('class-content-link');
+            const contactLink = document.getElementById('contact-link');
+            const choyleefutHistoryLink = document.getElementById('choyleefut-history-link');
+            const faqLink = document.getElementById('faq-link');
+            
+            if (location.pathname.includes('/zh') || location.pathname.includes('/zh-CN')) {
+                homeLink.href = 'index.html';
+                enrollLink.href = 'enroll.html';
+                localClassLink.href = 'index.html#schedule';
+                aboutSifuAngusLink.href = 'about-sifu-angus.html';
+                classContentLink.href = 'class-content.html';
+                contactLink.href = 'index.html#contact';
+                choyleefutHistoryLink.href = 'choyleefut-history.html';
+                faqLink.href = 'index.html#faq';
+            } else {
+                homeLink.href = '/index.html';
+                enrollLink.href = '/enroll.html';
+                localClassLink.href = '/index.html#schedule';
+                aboutSifuAngusLink.href = '/about-sifu-angus.html';
+                classContentLink.href = '/class-content.html';
+                contactLink.href = '/index.html#contact';
+                choyleefutHistoryLink.href = '/choyleefut-history.html';
+                faqLink.href = '/index.html#faq';
+            }
+
+        });
 });
